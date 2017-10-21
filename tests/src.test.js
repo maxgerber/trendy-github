bbf48755e0ae9887b2405ccf98d170410161c4f3
Max Gerber <mgerber94@gmail.com>
2017-10-21 18:18:58 +0100

    Wrote tests ensuring that GitHub's api is returning data successfully #5

--- tests/src.test.js
+++ tests/src.test.js
@@ -1,6 +1,8 @@
 const test = require('tape');
 const supertest = require('supertest');
 const app = require('../app');
+const nock = require('nock');
+const rp = require('request-promise-native');
 
 // Does tape work?
 test('1 equals 1', (t) => {
@@ -10,12 +12,33 @@ test('1 equals 1', (t) => {
 
 // Does supertest work?
 test('check if supertest works', (t) => {
-  supertest(app)
-    .get('/')
-    .expect(200)
-    .expect('Content-Type', /html/)
-    .end((err, res) => {
-      t.same(res.statusCode, 200, 'Status code is 200');
-      t.end();
-    })
-})
+  supertest(app).get('/').expect(200).expect('Content-Type', /html/).end((err, res) => {
+    t.same(res.statusCode, 200, 'Status code is 200');
+    t.end();
+  });
+});
+
+// GitHub API call returns json
+test('GitHub API returns JSON', (t) => {
+  const options = {
+    uri: 'https://api.github.com/search/repositories?q=created:%3E2017-01-10&',
+    qs: {
+      sort: 'stars',
+      order: 'desc',
+    },
+    headers: {
+      'User-Agent': 'Request-Promise',
+    },
+    json: true,
+  };
+
+  rp(options)
+    .then((response) => {
+      t.pass('GitHub has returned data successfully');
+      console.log(typeof response);
+      t.same(typeof response, 'object', 'Response is an object');
+    }).catch((err) => {
+      t.fail(`GitHub API has returned an error message: \n ${err}`);
+    });
+  t.end();
+});
